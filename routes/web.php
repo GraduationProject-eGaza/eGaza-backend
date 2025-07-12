e1a1075ab5ce9ab04018811cb133c4aae219be01
MuntahaSh <mnthyshbat@gmail.com>
2025-07-12 11:41:17 +0300

    tes not connection3

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,9 @@ Route::get('/clear-cache', function () {
     Artisan::call('config:cache');
     return 'Cache cleared successfully!';
 });
+Route::get('/check-env', function () {
+    return response()->json([
+        'app_key' => config('app.key'),
+        'env_key' => env('APP_KEY'),
+    ]);
+});
